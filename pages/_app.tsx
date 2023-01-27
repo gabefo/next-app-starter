18eb1ec7830b165f498d67d8f064660909cec90d
gabefo <gabrielfomiranda@gmail.com>
2023-01-27 20:00:50 -0300

    Configure eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import '@assets/globals.css'
+
+import { FC, ReactNode } from 'react'
 import type { AppProps } from 'next/app'
 
+const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const Layout = (Component as any).Layout || Noop
+
+  return (
+    <Layout pageProps={pageProps}>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
